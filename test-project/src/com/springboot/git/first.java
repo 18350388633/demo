a28c5d3ae3e6b10f8ade774e79a3640da6b62fc1
yangzhilin <18350388633@163.com>
2019-10-24 14:37:28 +0800

    本地工程添加到git

---
+++ test-project/src/com/springboot/git/first.java
@@ -0,0 +1,11 @@
+package com.springboot.git;
+
+public class first {
+    public static void main(String[] args) {
+        System.out.println("@@@@@@@@@@@@@@@@@@@@@@");
+        System.out.println("@@@@@@@@@@@@@@@@@@@@@@");
+        System.out.println("@@@@@@@@@@@@@@@@@@@@@@");
+        System.out.println("@@@@@@@@@@@@@@@@@@@@@@");
+        System.out.println("@@@@@@@@@@@@@@@@@@@@@@");
+    }
+}
